6457789f7e9c7083e2c49da3507e6173e8d0811e
ram_tanniru <ramtanniru2@gmail.com>
2024-08-06 11:21:58 +0530

    added filter function

--- src/app/productsGrid/page.jsx
+++ src/app/productsGrid/page.jsx
@@ -5,6 +5,9 @@ import { useEffect, useState } from "react";
 
 const ProductsGrid = () => {
   const [stickyMenu, setStickyMenu] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   // Sticky menu
   const handleStickyMenu = () => {
@@ -16,51 +19,63 @@ const ProductsGrid = () => {
   };
 
   useEffect(() => {
-    window.addEventListener("scroll", handleStickyMenu);
-  });
-    const [products, setProducts] = useState([]);
-    const [filteredProducts, setFilteredProducts] = useState([]);
-    const [searchQuery, setSearchQuery] = useState('');
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const response = await fetch('https://run.mocky.io/v3/200998b7-f48d-4456-a639-0b5d2d275c12');
-      const data = await response.json();
-      setProducts(data);
+    return () => {
+      window.removeEventListener("scroll", handleStickyMenu);
     };
-
-    fetchProducts().catch(console.error); 
   }, []);
 
-  const handleSearchChange = (event) => {
-    const query = event.target.value.toLowerCase();
-    setSearchQuery(query);
-    setFilteredProducts(products.filter(product => product.name.toLowerCase().includes(query)));
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(
+        "https://run.mocky.io/v3/200998b7-f48d-4456-a639-0b5d2d275c12"
+      );
+      const data = await response.json();
+      setProducts(data);
+      setFilteredProducts(data); 
+    };
+
+    fetchProducts().catch(console.error);
+  }, []);
+
+  const filterProducts = () => {
+    const filtered = products.filter((product) =>
+      product.item.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredProducts(filtered);
   };
 
+
+
+
   return (
-    <div className='my-100 bg-white'>
-      <header
-        className={`fixed left-0 top-0 z-99999 w-full bg-gray-100 py-4 ${
-          stickyMenu
-            ? "shadow-lg transition duration-100 "
-            : ""
-        }`}
-      >
+    <div className="my-100 bg-white min-h-screen">
+        <header
+          className={`fixed left-0 top-0 z-99999 w-full bg-gray-100 py-4 ${
+            stickyMenu ? "shadow-lg transition duration-100" : ""
+          }`}
+        >
           <div className="px-4 bg-gray-100">
-              <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center">
               <h1 className="text-2xl font-bold text-black">Explore</h1>
-              <button className="text-green-600 font-medium">Filter</button>
-              </div>
-              <input
+              <button
+                onClick={filterProducts}
+                className="text-green-600 font-medium"
+              >
+                Filter
+              </button>
+            </div>
+            <input
               type="text"
               placeholder="Search"
-              className="mt-4 p-2 w-full rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600"
-              />
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="mt-4 text-slate-700 p-2 w-full rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600"
+            />
           </div>
-      </header>
-        <div className="flex-grow p-4">
+        </header>
+        <div className="flex-grow p-4 mt-28">
             <div className="grid grid-cols-3 gap-4">
-              {products.map((product) => (
+              {filteredProducts.map((product) => (
                 <div key={product.id} className="flex flex-col items-start bg-white rounded-lg p-2">
                   <img
                     src="https://placehold.co/110x110.png"
--- src/app/productsList/page.jsx
+++ src/app/productsList/page.jsx
@@ -1,11 +1,12 @@
 "use client";
-import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const ProductsList = () => {
   const [stickyMenu, setStickyMenu] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
   // Sticky menu
   const handleStickyMenu = () => {
     if (window.scrollY >= 80) {
@@ -16,63 +17,81 @@ const ProductsList = () => {
   };
 
   useEffect(() => {
-    window.addEventListener("scroll", handleStickyMenu);
-  });
-    const [products, setProducts] = useState([]);
+    return () => {
+      window.removeEventListener("scroll", handleStickyMenu);
+    };
+  }, []);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch('https://run.mocky.io/v3/200998b7-f48d-4456-a639-0b5d2d275c12');
+      const response = await fetch(
+        "https://run.mocky.io/v3/200998b7-f48d-4456-a639-0b5d2d275c12"
+      );
       const data = await response.json();
       setProducts(data);
+      setFilteredProducts(data); 
     };
 
-    fetchProducts().catch(console.error); 
+    fetchProducts().catch(console.error);
   }, []);
+
+  const filterProducts = () => {
+    const filtered = products.filter((product) =>
+      product.item.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+  };
 
   return (
     <div className="my-100">
-        <header
-          className={`fixed left-0 top-0 z-99999 w-full bg-gray-100 py-4 ${
-            stickyMenu
-              ? "shadow-lg transition duration-100 "
-              : ""
-          }`}
-        >
-            <div className="px-4 bg-gray-100">
-                <div className="flex justify-between items-center">
-                <h1 className="text-2xl font-bold text-black">Explore</h1>
-                <button className="text-green-600 font-medium">Filter</button>
-                </div>
-                <input
-                type="text"
-                placeholder="Search"
-                className="mt-4 p-2 w-full rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600"
-                />
-            </div>
-        </header>
-        <div className="flex-grow bg-white">
-            <ul className="divide-y divide-gray-200">
-            {products.map((product) => (
-                <li key={product.createdAt} className="flex p-4">
-                <img
-                    src="https://placehold.co/50x50.png"
-                    alt={product.item}
-                    className="w-[50] h-[50] object-cover mb-2 rounded-md"
-                  />
-                <div className="ml-4 flex-grow">
-                    <h2 className="font-medium text-lg text-black">{product.item}</h2>
-                    <p className="text-gray-500">MRP: ₹{product.price}</p>
-                </div>
-                {product.shipping_method && (
-                    <div className="flex items-center justify-center">
-                    <span className="text-gray-400">{product.shipping_method}</span>
-                    </div>
-                )}
-                </li>
-            ))}
-            </ul>
+      <header
+        className={`fixed left-0 top-0 z-99999 w-full bg-gray-100 py-4 ${
+          stickyMenu ? "shadow-lg transition duration-100" : ""
+        }`}
+      >
+        <div className="px-4 bg-gray-100">
+          <div className="flex justify-between items-center">
+            <h1 className="text-2xl font-bold text-black">Explore</h1>
+            <button
+              onClick={filterProducts}
+              className="text-green-600 font-medium"
+            >
+              Filter
+            </button>
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="mt-4 text-slate-700 p-2 w-full rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600"
+          />
         </div>
+      </header>
+      <div className="flex-grow bg-white mt-28">
+        <ul className="divide-y">
+          {filteredProducts.map((product) => (
+            <li key={product.createdAt} className="flex p-4">
+              <img
+                src="https://placehold.co/50x50.png"
+                alt={product.item}
+                className="w-[50px] h-[50px] object-cover mb-2 rounded-md"
+              />
+              <div className="ml-4 flex-grow">
+                <h2 className="font-medium text-lg text-black">
+                  {product.item}
+                </h2>
+                <p className="text-gray-500">MRP: ₹{product.price}</p>
+              </div>
+              {product.shipping_method && (
+                <div className="flex items-center justify-center">
+                  <span className="text-gray-400">{product.shipping_method}</span>
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
